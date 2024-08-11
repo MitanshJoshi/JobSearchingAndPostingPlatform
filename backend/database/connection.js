ef7989fd8423b7aeb03e3220c588ce036dd3bf25
MitanshJoshi <mitansh.joshi141203@gmail.com>
2024-08-11 11:54:01 +0530

    databse

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
-export const connection=()=>{
-    mongoose.connect(process.env.MONGO_URI,{
-        dbName:"JOB_PORTAL"
-    }).then(()=>{
-        console.log("connected to the database");
-    }).catch((err)=>{
-        console.log(`Some error occured while connecting: ${err}`);
+export const connection = () => {
+    mongoose.connect(process.env.MONGO_URI, {
+        dbName: "JOB_PORTAL",
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 50000, 
     })
-}
+    .then(() => {
+        console.log("Connected to the database");
+    })
+    .catch((err) => {
+        console.log(`Some error occurred while connecting: ${err.message}`);
+    });
+};
