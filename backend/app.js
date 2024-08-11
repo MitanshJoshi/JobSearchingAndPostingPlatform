ea5d48a1a88718f802b8e034175dcd809f58eadf
MitanshJoshi <mitansh.joshi141203@gmail.com>
2024-08-11 12:23:30 +0530

    mongouri

--- backend/app.js
+++ backend/app.js
@@ -1,42 +1,55 @@
-import express from "express"
-import {config} from "dotenv"
-import cors from "cors"
-import userRouter from "./routes/userRouter.js"
-import jobRouter   from "./routes/jobRouter.js"
-import applicationRouter   from "./routes/applicationRouter.js"
-import {connection} from "./database/connection.js "
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import userRouter from "./routes/userRouter.js";
+import jobRouter from "./routes/jobRouter.js";
+import applicationRouter from "./routes/applicationRouter.js";
+
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
 import fileUpload from "express-fileupload";
-import { newsLetterCron } from "./automation/automationCron.js"
+import { newsLetterCron } from "./automation/automationCron.js";
 const app = express();
 
-config({path: "./config/config.env"})
+config({ path: "./config/config.env" });
 
-app.use(cors({
-    origin:[process.env.FRONTEND_URL],
-    methods:["GET","POST","PUT","DELETE"],
-    credentials:true,
-})
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
 );
 
 console.log(process.env.MONGO_URI);
 
-
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(fileUpload({
-    useTempFiles:true,
-    tempFileDir:"/tmp/"
-}))
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
-app.use("/api/V1/user", userRouter)
-app.use("/api/V1/job", jobRouter)
-app.use("/api/V1/application", applicationRouter)
-newsLetterCron(); 
-connection(); 
-app.use(errorMiddleware)
+app.use("/api/V1/user", userRouter);
+app.use("/api/V1/job", jobRouter);
+app.use("/api/V1/application", applicationRouter);
+newsLetterCron();
+mongoose
+  .connect("mongodb://127.0.0.1:27017/JOB_PORTAL", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 50000,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Failed to connect:", err.message);
+  });
+app.use(errorMiddleware);
 
 export default app;
